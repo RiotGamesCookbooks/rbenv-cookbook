ba4071b4576383c66f85234eb36877e24805b0c2
Jamie Winsor <jamie@vialstudios.com>
2011-12-17 18:54:42 -0800

    fix issue where os dependencies were written as cookbook dependencies

--- metadata.rb
+++ metadata.rb
@@ -2,10 +2,10 @@ maintainer       "Riot Games"
 maintainer_email "jwinsor@riotgames.com"
 license          "Apache 2.0"
 description      "Installs and configures rbenv"
-version          "0.9.1"
+version          "0.9.2"
 
 %w{ centos redhat fedora }.each do |os|
-  depends os
+  supports os
 end
 
 %w{ git }.each do |cb|
