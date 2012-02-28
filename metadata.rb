dc992a29d7f72adbbb966cfad17e745940391f17
Trotter Cashion <cashion@gmail.com>
2012-02-27 21:40:16 -0800

    Add ubuntu/debian support.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ license          "Apache 2.0"
 description      "Installs and configures rbenv"
 version          "1.0.1"
 
-%w{ centos redhat fedora }.each do |os|
+%w{ centos redhat fedora ubuntu debian }.each do |os|
   supports os
 end
 
