685fb45b396843adffe3147acfb43745e11fc5cf
Jamie Winsor <jamie@vialstudios.com>
2012-03-30 11:30:18 -0700

    bump version to 1.1.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Riot Games"
 maintainer_email "jamie@vialstudios.com"
 license          "Apache 2.0"
 description      "Installs and configures rbenv"
-version          "1.0.2"
+version          "1.1.0"
 
 %w{ centos redhat fedora ubuntu debian }.each do |os|
   supports os
