f8cf4c105962a8fb26e4d24ca7a52560788bfd9d
Jamie Winsor <jamie@vialstudios.com>
2012-04-03 01:07:50 -0700

    add metadata for recipes

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,11 @@ license          "Apache 2.0"
 description      "Installs and configures rbenv"
 version          "1.1.0"
 
+recipe "rbenv", "Delegates to recipe[rbenv::system_install]"
+recipe "rbenv::system_install", "Configures a node with a system wide rbenv and ruby_build installation accessible by users in the rbenv group"
+recipe "rbenv::ruby_build", "Installs ruby_build to a node which enables rbenv the ability to install and manage versions of Ruby"
+recipe "rbenv::ohai_plugin", "Installs an rbenv Ohai plugin onto the node to automatically populate attributes about the rbenv installation"
+
 %w{ centos redhat fedora ubuntu debian }.each do |os|
   supports os
 end
