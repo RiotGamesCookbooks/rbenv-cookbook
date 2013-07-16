033b5c281552458c394d651634088379a3f839cc
Jamie Winsor <jamie@vialstudios.com>
2013-07-16 01:24:40 -0700

    version bump 1.5.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Riot Games"
 maintainer_email "jamie@vialstudios.com"
 license          "Apache 2.0"
 description      "Installs and configures rbenv"
-version          "1.4.2"
+version          "1.5.0"
 
 recipe "rbenv", "Installs and configures rbenv"
 recipe "rbenv::ruby_build", "Installs and configures ruby_build"
