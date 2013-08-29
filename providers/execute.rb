ff36b029eb3830c64f9a64ef98c13e18d158fdcd
Jamie Winsor <jamie@vialstudios.com>
2013-08-29 14:08:15 -0700

    properly set path in execute resource

--- providers/execute.rb
+++ providers/execute.rb
@@ -10,23 +10,24 @@ include Chef::Mixin::Rbenv
 def load_current_resource
   @path                         = [ rbenv_shims_path, rbenv_bin_path ] + new_resource.path + system_path
   @environment                  = new_resource.environment
+  @environment["PATH"]          = @path.join(":")
   @environment["RBENV_ROOT"]    = rbenv_root_path
   @environment["RBENV_VERSION"] = new_resource.ruby_version if new_resource.ruby_version
 
-  new_resource.path(@path)
   new_resource.environment(@environment)
 end
 
 action :run do
-  execute "eval \"$(rbenv init -)\""
+  execute "eval \"$(rbenv init -)\"" do
+    environment new_resource.environment
+  end
 
-  exec_resource = execute new_resource.name do
+  execute new_resource.name do
     command     new_resource.command
     creates     new_resource.creates
     cwd         new_resource.cwd
     environment new_resource.environment
     group       new_resource.group
-    path        new_resource.path
     returns     new_resource.returns
     timeout     new_resource.timeout
     user        new_resource.user
