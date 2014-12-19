a5d2352206a59328a4ea95c6b224a4fb9e461f1d
Aleksandar Diklic <rastasheep3@gmail.com>
2014-12-19 18:56:15 +0100

    Use lazy evaluation for the derived attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -26,8 +26,8 @@ default[:rbenv][:group_users]    = Array.new
 default[:rbenv][:git_repository] = "https://github.com/sstephenson/rbenv.git"
 default[:rbenv][:git_revision]   = "master"
 default[:rbenv][:install_prefix] = "/opt"
-default[:rbenv][:root_path]      = "#{node[:rbenv][:install_prefix]}/rbenv"
-default[:rbenv][:user_home]      = "/home/#{node[:rbenv][:user]}"
+default[:rbenv][:root_path]      = "%{node[:rbenv][:install_prefix]}/rbenv"
+default[:rbenv][:user_home]      = "/home/%{node[:rbenv][:user]}"
 
 default[:ruby_build][:git_repository] = "https://github.com/sstephenson/ruby-build.git"
 default[:ruby_build][:git_revision]   = "master"
