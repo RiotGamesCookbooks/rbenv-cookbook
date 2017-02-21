db6da955ee1d717db182f8b7e2068d997cf1ca3e
Tom Ward <tomward@fmail.co.uk>
2017-02-21 20:30:08 +0000

    Version 1.8.1 (#8)

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Riot Games"
 maintainer_email "jamie@vialstudios.com"
 license          "Apache 2.0"
 description      "Installs and configures rbenv"
-version          "1.8.0"
+version          "1.8.1"
 
 recipe "rbenv", "Installs and configures rbenv"
 recipe "rbenv::ruby_build", "Installs and configures ruby_build"
