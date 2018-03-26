66f32f6dc93e2d866e4c4b7f0baa287adb551a21
Tom Ward <tomward@fmail.co.uk>
2018-03-26 11:53:49 +0100

    Remove use of node.set (#11)
    * deprecated in Chef 13
    * I think `normal` will work, alternatively we could use `override`

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,9 +19,9 @@
 # limitations under the License.
 #
 
-node.set[:rbenv][:root]          = rbenv_root_path
-node.set[:ruby_build][:prefix]   = "#{node[:rbenv][:root]}/plugins/ruby_build"
-node.set[:ruby_build][:bin_path] = "#{node[:ruby_build][:prefix]}/bin"
+node.normal[:rbenv][:root] = rbenv_root_path
+node.normal[:ruby_build][:prefix] = "#{node[:rbenv][:root]}/plugins/ruby_build"
+node.normal[:ruby_build][:bin_path] = "#{node[:ruby_build][:prefix]}/bin"
 
 case node[:platform]
 when "ubuntu", "debian"
