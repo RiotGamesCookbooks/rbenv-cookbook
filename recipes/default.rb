4356a0892c87aa5bd6145b86ce85b86aa20b50b2
achaitanyajami <arun.chaitanyajami@news.co.uk>
2020-05-07 20:54:24 +0530

    undefined method set for #<Chef::Node::Attribute:0x0000000003bfb2a8>

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,9 +19,9 @@
 # limitations under the License.
 #
 
-node.set[:rbenv][:root]          = rbenv_root_path
-node.set[:ruby_build][:prefix]   = "#{node[:rbenv][:root]}/plugins/ruby_build"
-node.set[:ruby_build][:bin_path] = "#{node[:ruby_build][:prefix]}/bin"
+node.force_default[:rbenv][:root]          = rbenv_root_path
+node.force_default[:ruby_build][:prefix]   = "#{node[:rbenv][:root]}/plugins/ruby_build"
+node.force_default[:ruby_build][:bin_path] = "#{node[:ruby_build][:prefix]}/bin"
 
 case node[:platform]
 when "ubuntu", "debian"
